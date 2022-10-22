5ab284e6e0568462654e3ceb05ffd44bb103a634
Ailton Ferreira Luz <djailtim@gmail.com>
2022-10-21 20:51:26 -0400

    refactoring messages printed

--- src/App.java
+++ src/App.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.Arrays;
 
 public class App {
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -9,9 +10,12 @@ public class App {
         Double[][] temperatures = CalculateTemperatures.conversions(quantity,temperatureUnit1, temperatureUnit2);
 
         Double averageTypedTemperatures = CalculateTemperatures.calculateAverage(temperatures[0]);
-        Display.printMessage("\n\nA média das temperaturas digitadas é: %.2f", averageTypedTemperatures);
+        Display.printMessage("\nA média das temperaturas digitadas é: %.2f", averageTypedTemperatures);
+        Display.printMessage("\nAs temperaturas digitadas foram: " + Arrays.toString(temperatures[0]));
 
         Double averageConvertedTemperatures = CalculateTemperatures.calculateAverage(temperatures[1]);
         Display.printMessage("\nA média das temperaturas convertidas é: %.2f", averageConvertedTemperatures);
+        Display.printMessage("\nO resultado das temperaturas convertidas foram: " + Arrays.toString(temperatures[1]));
+
     }
 }
